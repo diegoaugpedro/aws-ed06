4bf4406283082d9ff42320c6ca56a2a2efb0bc71
Diego Augusto Pedro <diegoaugpedro@gmail.com>
2020-12-14 23:58:48 -0300

    Includes files

---
+++ APIGateway/url.js
@@ -0,0 +1,4 @@
+module.exports = {
+    SALES_API_URL: 'https://app.swaggerhub.com/apis/diegoaugpedro/Venda/1.0.0',
+    PRODUCTS_API_URL: 'https://app.swaggerhub.com/apis/diegoaugpedro/Produto/1.0.0'
+};
